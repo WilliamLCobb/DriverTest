ed028fbecd80f077d42778d25e71fac35fe049a9
Will Cobb <willlcobb@gmail.com>
2016-11-03 14:36:20 -0500

    Car moves

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour : MonoBehaviour {
+	public bool running;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (running) {
+			transform.Translate(new Vector3(0,0, -speed * Time.deltaTime));
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate (Vector3.up, Time.deltaTime * -50);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate (Vector3.up, Time.deltaTime * 50);
+		}
+	}
+}
---
+++ Assets/Scripts/MainCameraBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraBehaviour : MonoBehaviour {
+	bool running = false;
+	int speed = 10;
+
+	public CarBehaviour car;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnGUI() {
+		// Tea Cup
+		if (GUI.Button (new Rect (10, 50, 120, 30), "Go!")) {
+			running = true;
+			car.running = true;
+			car.speed = speed;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (running) {
+			transform.Translate(new Vector3(0,0, -speed * Time.deltaTime), Space.World);
+		}
+	}
+}
